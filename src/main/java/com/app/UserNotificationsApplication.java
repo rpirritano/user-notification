77f17026462b23f4dbc76f5d561b1e8a209e401d
Robert Pirritano <rpirritano@gmail.com>
2020-06-02 14:20:42 -0700

    first commit

---
+++ src/main/java/com/app/UserNotificationsApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserNotificationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserNotificationsApplication.class, args);
+	}
+
+}
